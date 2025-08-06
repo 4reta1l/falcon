4c3d1b7217c39b69e0356c9f898023a09b27a502
Maksym Pyvovarov <maxpyvovarov@gmail.com>
2025-08-06 02:09:34 +0200

    Delete storyboard, set new root

---
+++ Sources/Screens/AppView.swift
@@ -0,0 +1,16 @@
+//
+//  AppView.swift
+//  falcon
+//
+//  Created by Maksym Pyvovarov on 06/08/2025.
+//
+
+import SwiftUI
+
+struct AppView: View {
+
+    var body: some View {
+        Text("You are running the Falcon app!")
+    }
+}
+
